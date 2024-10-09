d57068a87fac9b622bcd6c4a220a2ee48024699d
shivammathapati <156904099+shivam6967@users.noreply.github.com>
2024-10-10 00:45:15 +0530

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "AI NEXUS",
+  description: "This is my Accenture Hackathon Project",
 };
 
 export default function RootLayout({
